4475e01604e9a3d0c5f7d22ce0b6d7c1134dc5b4
belinko@gmail.com <belinko@gmail.com@a90e2f76-522e-f701-662c-e68404131795>
2013-10-24 23:58:00 +0000

    Initial checkout of the game logic module.

---
+++ jateklogika/RoboTicTacToe/IGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoboTicTacToe
+{
+    /// <summary>
+    /// Enumeration to define piece types.
+    /// </summary>
+    public enum Piece { X, O };
+
+    /// <summary>
+    /// The "main" interface for the communication. Everything shoud go here, that all modules must to know.
+    /// </summary>
+    interface IGame
+    {
+    }
+}
---
+++ jateklogika/RoboTicTacToe/IKinematics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RoboTicTacToe
+{
+    public enum RobotMovement { Cheer, Grieve };
+    public enum RobotStatus { Ready, Moving, Offline, ServoError, OutOfPieces };
+
+    /// <summary>
+    /// This interface represents the communication between the kinematics and the game logic modules.
+    /// </summary>
+    interface IKinematics : IGame
+    {
+        void NewGame(byte colCount, byte rowCount);
+        void PlacePiece(Piece pieceType, byte destCol, byte destRow);
+        void Move(RobotMovement movementType);
+
+        event EventHandler RobotStatusChanged;
+    }
+
+    class RobotStatusChangedEventArgs
+    {
+        private RobotStatus CurrentStatus { get; set; }
+
+        public RobotStatusChangedEventArgs(RobotStatus newStatus)
+        {
+            this.CurrentStatus = newStatus;
+        }
+    }
+}
